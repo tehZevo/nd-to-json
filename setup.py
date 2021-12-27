b0749ed183112b1c7c6ba6d4e1aa7a3e9a9d76a9
tehZevo <tehzevo@users.noreply.github.com>
2021-12-26 23:36:44 -0500

    Add orjson requirement

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(name='nd_to_json',
   version='0.0.0',
   install_requires = [
-    'numpy',
+    "numpy",
+    "orjson"
   ],
   packages=find_packages())
