4c5a0c15b3c051f316953c5abf6d8076072c65c5
tehZevo <tehzevo@users.noreply.github.com>
2021-12-26 23:13:24 -0500

    Remove b64gzip method

--- benchmark.py
+++ benchmark.py
@@ -2,19 +2,13 @@ import numpy as np
 import time
 import json
 
-from nd_to_json import nd_to_json, json_to_nd, serialize, deserialize
-
-def print_hex(bs, n=8):
-    bs = bs.hex()
-    bs = [bs[i:i+n] for i in range(0, len(bs), n)]
-    print(bs)
+from nd_to_json import nd_to_json, json_to_nd
 
 #tests:
 N = 10
 TRIALS = 100
 DIMS = [1, 2, 3, 4, 5]
-B64 = [True, False]
-GZIP = [True, False]
+METHODS = ["orjson", "np_save", "plain"]
 
 def dim_shape(dim):
     return [N for i in range(dim)]
@@ -23,37 +17,25 @@ def create_r(dim):
     #return np.random.normal(0, 1, dim_shape(dim))
     return np.zeros(dim_shape(dim))
 
-def do_run(dim, do_b64, do_gzip):
+def do_run(dim, method):
     r = create_r(dim)
-    r = nd_to_json(r, compress=True, do_gzip=do_gzip, do_b64=do_b64)
+    r = nd_to_json(r, method)
 
     r = json.dumps(r)
     r = json.loads(r)
 
     #now decode
-    r = json_to_nd(r, compress=True, do_gzip=do_gzip, do_b64=do_b64)
+    r = json_to_nd(r, method)
 
 for dim in DIMS:
     print("dim", dim, "values", np.prod(dim_shape(dim)))
-    #old method tests
-    times = []
-    for _ in range(TRIALS):
-        start_time = time.time()
-        r = create_r(dim)
-        r = json.dumps(nd_to_json(r, compress=False))
-        r = json_to_nd(json.loads(r), compress=False)
-        times.append(time.time() - start_time)
-    avg_time = np.mean(times)
-    print("OLD", ":", avg_time)
-
     #new method tests
-    for do_b64 in B64:
-        for do_gzip in GZIP:
-            times = []
-            for _ in range(TRIALS):
-                start_time = time.time()
-                do_run(dim, do_b64, do_gzip)
-                times.append(time.time() - start_time)
-            avg_time = np.mean(times)
-            print("NEW", "do_b64", do_b64, "do_gzip", do_gzip, ":", avg_time)
+    for method in METHODS:
+      times = []
+      for _ in range(TRIALS):
+        start_time = time.time()
+        do_run(dim, method)
+        times.append(time.time() - start_time)
+      avg_time = np.mean(times)
+      print(f"Method: {method}: {avg_time}s")
     print()
--- nd_to_json/__init__.py
+++ nd_to_json/__init__.py
@@ -1,53 +1,31 @@
 import numpy as np
 import zlib
 import base64
+import io
+import orjson
 
-ENDIAN = "<"
+def nd_to_json(r, method="orjson"):
+  if method == "orjson":
+    return orjson.dumps(r, option=orjson.OPT_SERIALIZE_NUMPY).decode("utf8")
+  if method == "np_save":
+    b = io.BytesIO()
+    np.save(b, r)
+    b.seek(0)
+    b = b.read()
+    b = base64.encodebytes(b)
+    b = b.decode('utf8')
+    return b
+  #plain method
+  return {"data": r.flatten().tolist(), "shape": r.shape}
 
-def nd_to_json(r, compress=True, do_gzip=True, do_b64=True):
-    if compress:
-        return encode(serialize(r), do_gzip, do_b64)
-    return {"data": r.flatten().tolist(), "shape": r.shape}
-
-def json_to_nd(r, compress=True, do_gzip=True, do_b64=True):
-    if compress:
-        return deserialize(decode(r, do_gzip, do_b64))
-    return np.reshape(r["data"], r["shape"])
-
-def encode(r, do_gzip=True, do_b64=True):
-    if do_gzip:
-        r = zlib.compress(r)
-    r = base64.b64encode(r).decode("utf-8") if do_b64 else r.hex()
-    return r
-
-def decode(r, do_gzip=True, do_b64=True):
-    r = base64.b64decode(r) if do_b64 else bytearray.fromhex(r)
-    if do_gzip:
-        r = zlib.decompress(r)
-    return r
-
-def serialize(r):
-    #data will always be 32bit big endian floats
-    r = r.astype(ENDIAN+"f4")
-    #convert length of shape to int32 bytes
-    lenshape = np.array(len(r.shape), dtype=ENDIAN+"i4").tobytes()
-    #convert shape to int32 bytes
-    shape = np.array(r.shape, dtype=ENDIAN+"i4").tobytes()
-    #convert data to bytes
-    r = r.flatten().tobytes()
-    #concat
-    data = lenshape + shape + r
-
-    return data
-
-def deserialize(r):
-    #read lenshape
-    lenshape = int(np.frombuffer(r[:4], dtype=ENDIAN+"i4"))
-    #read shape
-    shape = np.frombuffer(r[4:4+lenshape*4], dtype=ENDIAN+"i4")
-    #read data
-    data = np.frombuffer(r[4+lenshape*4:], dtype=ENDIAN+"f4")
-    #reshape data
-    data = np.reshape(data, shape)
-
-    return data
+def json_to_nd(r, method="orjson"):
+  if method == "orjson":
+    return orjson.loads(r)
+  if method == "np_save":
+    b = r.encode('utf8')
+    b = base64.decodebytes(b)
+    b = io.BytesIO(b)
+    b = np.load(b)
+    return b
+  #plain method
+  return np.reshape(r["data"], r["shape"])
